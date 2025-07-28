4887abe9ef97402be2bd1279bfe571744ed0c96e
Maizer Gomes <maizer.gomes@gmail.com>
2025-07-28 07:49:13 +0200

    Fix binary url

--- Formula/madzi-cli.rb
+++ Formula/madzi-cli.rb
@@ -1,16 +1,16 @@
 class MadziCli < Formula
   desc "CLI tool to manage Madzi postpaid water services"
   homepage "https://github.com/MaizerGomes/madzi-cli"
-  url "https://github.com/MaizerGomes/madzi-cli/releases/download/v0.1.0/madzi-cli-darwin-amd64"
+  url "https://github.com/MaizerGomes/madzi-cli/releases/download/v0.1.0/madzi-cli-darwin-arm64"
   sha256 "dfa4007d01fc30b790737fe1e02d6ef1ac82695f0ec2cb2b3d7e720bc6658676"
   license "MIT"
   version "0.1.0"
 
   def install
-    bin.install "madzi-cli"
+    bin.install "madzi-cli-darwin-arm64" => "madzi-cli"
   end
 
   test do
-    assert_match "Madzi CLI", shell_output("#{bin}/madzi-cli --help")
+    system "#{bin}/madzi-cli", "--help"
   end
 end
