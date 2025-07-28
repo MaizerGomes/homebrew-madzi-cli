da42436af2413702397d673a7124228588df4cdb
Maizer Gomes <maizer.gomes@gmail.com>
2025-07-28 22:12:02 +0200

    Launch first production version

--- Formula/madzi-cli.rb
+++ Formula/madzi-cli.rb
@@ -1,10 +1,10 @@
 class MadziCli < Formula
   desc "CLI tool to manage Madzi postpaid water services"
   homepage "https://github.com/MaizerGomes/madzi-cli"
-  url "https://github.com/MaizerGomes/madzi-cli/releases/download/v0.1.0/madzi-cli-darwin-arm64"
-  sha256 "dfa4007d01fc30b790737fe1e02d6ef1ac82695f0ec2cb2b3d7e720bc6658676"
+  url "https://github.com/MaizerGomes/madzi-cli/releases/download/v1.0.0/madzi-cli-darwin-arm64"
+  sha256 "0e1bfa72886b303c3059ca54fafa9a02691ff04de126a231cee3cb67b7c9aaad"
   license "MIT"
-  version "0.1.0"
+  version "1.0.0"
 
   def install
     bin.install "madzi-cli-darwin-arm64" => "madzi-cli"
